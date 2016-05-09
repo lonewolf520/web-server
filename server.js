a007cbc520438e8d6e7c674621b30f6f870e275f
Lone Wolf <seagull826@gmail.com>
2016-05-09 13:00:54 -0400

    update language

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var middleware = require('./middleware.js')
 app.use(middleware.logger);
 
 app.get('/about',middleware.requireAuthentication, function(req, res) {
-    res.send('About US!');
+    res.send('About US');
 });
 // express.static is used to get the entire folder.in my case it is public
 /*console.log(__dirname);*/ 
